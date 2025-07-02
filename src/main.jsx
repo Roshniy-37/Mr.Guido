967bf9ebb199d2da12899b5d3d85eb6a21ecf88a
roshni <roshni.23545@sscbs.du.ac.in>
2025-07-03 01:03:10 +0530

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter as Router} from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
     <Auth0Provider
       domain="dev-p0frrosaxvx7uzan.us.auth0.com"
       clientId="vETkLgvuFwiz0RBX0bCNZAbOIhQxIRga"
@@ -15,8 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         redirect_uri: window.location.origin
       }}
     >
-      <App />
-      </Auth0Provider>
-    </Router>
-  </React.StrictMode>,
-)
+    <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
